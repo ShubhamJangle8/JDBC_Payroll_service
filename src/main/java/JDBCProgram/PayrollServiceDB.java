60413da62484473c87069b9bd30e539274225b3d
jangle <shubhamjangale0000@gmail.com>
2020-11-01 10:34:30 +0530

    Added : Added functionality to establish and check connection between Java and Database

--- src/main/java/JDBCProgram/PayrollServiceDB.java
+++ src/main/java/JDBCProgram/PayrollServiceDB.java
@@ -1,7 +1,40 @@
 package JDBCProgram;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
 public class PayrollServiceDB {
 	public static void main(String[] args) {
 		System.out.println("Welcome to JDBC 1st Program");
+		String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+		String userName = "root";
+		String password = "1234";
+		Connection connection;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver Loaded");
+		}
+		catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		listDrivers();
+		try {
+			System.out.println("Connecting to database : " + jdbcURL);
+			connection = DriverManager.getConnection(jdbcURL, userName, password); 
+			System.out.println("Connection is successful!!!!" + connection);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void listDrivers() {
+		Enumeration<Driver> driverList = DriverManager.getDrivers();
+		while(driverList.hasMoreElements()) {
+			Driver driverClass = (Driver) driverList.nextElement();
+			System.out.println("   " + driverClass.getClass().getName());
+		}
 	}
 }

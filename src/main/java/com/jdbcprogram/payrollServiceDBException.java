08c52e397b2ea90d78f0f71e9e120961f5de7309
jangle <shubhamjangale0000@gmail.com>
2020-11-10 15:53:46 +0530

    Refactored : Added the Custom Exception

---
+++ src/main/java/com/jdbcprogram/payrollServiceDBException.java
@@ -0,0 +1,8 @@
+package com.jdbcprogram;
+
+@SuppressWarnings("serial")
+public class payrollServiceDBException extends Exception {
+	public payrollServiceDBException(String message) {
+		super(message);
+	}
+}

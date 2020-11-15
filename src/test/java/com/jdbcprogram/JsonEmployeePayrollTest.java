c5b8145cef5804b97fac8197d97a43f12c7c73e6
jangle <shubhamjangale0000@gmail.com>
2020-11-15 09:49:13 +0530

    Modified : Modified the add function to add Multiple Employees

--- src/test/java/com/jdbcprogram/JsonEmployeePayrollTest.java
+++ src/test/java/com/jdbcprogram/JsonEmployeePayrollTest.java
@@ -1,7 +1,10 @@
 package com.jdbcprogram;
 
+import static org.junit.Assert.assertEquals;
+
 import java.time.LocalDate;
 import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -62,6 +65,34 @@ public class JsonEmployeePayrollTest {
 		employeePayrollService.addEmployeeToPayroll(employeePayrollData, IOService.REST_IO);
 		long entries = employeePayrollService.countEntries(IOService.REST_IO);
 		Assert.assertEquals(7, entries);
+	}
+	
+	@Test
+	public void givenMultipleNewEmployees_WhenAdded_ShouldMatch201ResponseAndCount() {
+		EmployeePayroll[] arrayOfEmp = getEmployeeList();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmp));
+		EmployeePayroll[] arrayOfEmployee = { new EmployeePayroll(0, "Julia", "F", 6000000.0, LocalDate.now()),
+				new EmployeePayroll(0, "Joe", "M", 7000000.0, LocalDate.now()),
+				new EmployeePayroll(0, "Kamala", "F", 5000000.0, LocalDate.now()) };
+		List<EmployeePayroll> employeeList = Arrays.asList(arrayOfEmployee);
+		employeeList.forEach(employee -> {
+			Runnable task = () -> {
+				Response response = addEmployeeToJsonServer(employee);
+				int statusCode = response.getStatusCode();
+				assertEquals(404, statusCode);
+				EmployeePayroll newEmployee = new Gson().fromJson(response.asString(), EmployeePayroll.class);
+				employeePayrollService.addEmployeeToPayroll(newEmployee);
+			};
+			Thread thread = new Thread(task, employee.name);
+			thread.start();
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		long count = employeePayrollService.countEntries(IOService.REST_IO);
+		assertEquals(9, count);
 	}
 
 }
